010a7752cad601211e1232b64ce22f99d657fc76
Ramuel Gonzalez <ramuelcl@gmail.com>
2024-07-28 20:23:56 +0200

    instalando laravel-permission de Spatie

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,17 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-      {{ __("Dashboard") }}
-    </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-  <div class="py-12">
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
 
-        {{-- @livewire("forms.paises") --}}
-        <x-tarjeta titulo="Mi Tarjeta">
-          {{-- @livewire("contador") --}}
-          @livewire("posts.lwlistaposts", [
-              "title" => "hola mundo desde la vista",
-              "user" => 1,
-          ])
-        </x-tarjeta>
-      </div>
+                @livewire('forms.paises')
+
+            </div>
+        </div>
     </div>
-  </div>
 </x-app-layout>
